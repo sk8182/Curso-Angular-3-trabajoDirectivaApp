fc6393d584bdaeb3c7ce413fbafa211626298784
Julio Martinez <sk8182enkidu@gmail.com>
2024-10-09 17:59:41 +0200

    Curso Angular. Directivas II. Vídeo 15 - cómo utilizar un else con la directiva *ngIF y vemos también cómo utilizar la directiva *ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,24 @@ export class AppComponent {
   titulo = 'Registro de usuarios';
   mensaje = "";
   registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas: { titulo: string }[];
+
+
+  constructor(){
+
+
+    this.entradas =[
+
+      {titulo:"Python cada día más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"JS cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus apps"},
+      {titulo:"¿Dónde quedó pascal?"}
+    ]
+
+  }
 
   registrarUsuario(){
     
